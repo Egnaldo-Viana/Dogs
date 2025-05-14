3fd53feab596627dd6f72de7781cae8510c78fda
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-05-13 21:07:46 -0300

    validate token

--- src/api.jsx
+++ src/api.jsx
@@ -1,5 +1,6 @@
 export const API_URL = 'https://dogsapi.origamid.dev/json';
 
+// Solicita o token do usuario após autenticação
 export function TOKEN_POST(body) {
   return {
     url: API_URL + '/jwt-auth/v1/token',
@@ -13,6 +14,20 @@ export function TOKEN_POST(body) {
   };
 }
 
+// Verifica se o token é valido
+export function TOKEN_VALIDATE_POST(token) {
+  return {
+    url: API_URL + '/jwt-auth/v1/token/validate',
+    options: {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    },
+  };
+}
+
+// Solicita os dados do usuário após informar o token
 export function USER_GET(token) {
   return {
     url: API_URL + '/api/user',
