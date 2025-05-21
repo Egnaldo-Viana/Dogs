dc390ba1f35d7875b4ac7bc7e562315db9a3baca
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-05-21 00:27:29 -0300

    Comentario Post

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentForm from './PhotoCommentForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
-  return <div></div>;
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentForm id={props.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
